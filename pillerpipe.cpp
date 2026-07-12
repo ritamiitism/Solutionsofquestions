6755f323d0a44f58c6cd4ac3769db18663d5cba5
RK <142689034+ritamiitism@users.noreply.github.com>
2026-07-12 18:22:49 +0530

    Create pillerpipe.cpp

---
+++ pillerpipe.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define scd second
+#define foi(i,n) for(ll i=0;i<n;i++) 
+
+
+void bfs(vector<vector<ll>>&graph,ll s){
+ queue<ll>q;
+ vector<bool>visited(graph.size(),false);
+ q.push(s);
+ visited[s]=true;
+ while(!q.empty()){
+  ll node=q.front();
+  q.pop();
+  for(ll neighbor:graph[node]){
+   if(!visited[neighbor]){
+    visited[neighbor]=true;
+    q.push(neighbor);
+   }
+  }
+ }
+}
+
+ll gcd(ll a,ll b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+
+ll lcm(ll a,ll b){
+    return (a/gcd(a,b))*b;
+}
+
+bool compareBySecond(pair<ll,ll> a,pair<ll,ll> b){
+    return a.scd<b.scd;
+}
+
+ll binarySearchEqualOrLess(vector<ll>& arr,ll target){
+    ll low=0,high=arr.size()-1,result=-1;
+    while(low<=high){
+        ll mid=low+(high-low)/2;
+        if(arr[mid]<=target){
+            result=mid;
+            low=mid+1;
+        }
+        else high=mid-1;
+    }
+    return result;
+}
+
+ll binarySearch(const vector<ll>& arr,ll target){
+    ll left=0,right=arr.size()-1;
+    while(left<=right){
+        ll mid=left+(right-left)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]<target) left=mid+1;
+        else right=mid-1;
+    }
+    return -1;
+}
+
+string ask(ll n){
+    cout<<n<<endl;
+    string s;
+    cin>>s;
+    return s;
+}
+
+ll nextPowerOfTwo(unsigned ll n){
+    if(n==0) return 1;
+    if((n&(n-1))==0) return n;
+    n|=(n>>1);
+    n|=(n>>2);
+    n|=(n>>4);
+    n|=(n>>8);
+    n|=(n>>16);
+    n|=(n>>32);
+    return n+1;
+}
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+
+        int d=accumulate(a.begin(),a.end(),0);
+
+        vector<vector<int>>dp(n+1,vector<int>(d+1,-1));
+
+        dp[0][0]=0;
+
+
+        for(int i=1;i<=n;i++)
+        {
+            int x=a[i-1];
+            for(int j=0;j<=d;j++)
+            {
+                if(dp[i-1][j]==-1)
+                {
+                    continue;
+                }
+                dp[i][j]=max(dp[i][j],dp[i-1][j]);
+
+                if(j+x<=d)
+                dp[i][j+x]=max(dp[i][j+x],dp[i-1][j]+x);
+
+                if(j-x>=0)
+                {
+                    dp[i][j-x]=max(dp[i][j-x],dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][x-j]=max(dp[i][x-j],dp[i-1][j]+(x-j));
+                }
+
+
+            }
+        }
+        cout<<dp[n][0]<<endl;
+
+
+
+    }
+}
